11078a61495414752d44b5c1d397fa14f48ec37b
byhoson <byhoson@postech.ac.kr>
2025-12-14 22:55:22 +0900

    DMC2: define ISystem = System + init

--- Bakery/DMC2.lean
+++ Bakery/DMC2.lean
@@ -3,13 +3,14 @@ import Mathlib.Logic.Relation
 
 universe u
 
+--- System: Conf + step
 class System (σ : Type u) where
   step : σ → σ → Prop
 
----namespace System
+--- ISystem: System + init
+class ISystem (σ : Type u) [System σ] where
+  init : σ → Prop
 
--- so you can write `c ⇒ c'` once an instance is available
----export System (step)
 infix:110 " ⇒ " => System.step
 
 /-- Reflexive-transitive closure of `⇒`. -/
@@ -17,6 +18,13 @@ abbrev steps {σ : Type u} [System σ] : σ → σ → Prop :=
   Relation.ReflTransGen (System.step (σ := σ))
 
 infix:110 " ⇒* " => steps
+
+
+-- class IndInv {σ : Type u} [System σ] (P : σ → Prop) : Prop where
+--   base : ∀ {c : σ}, P c → P c
+--   inv : ∀ {c c' : σ}, P c → (c ⇒ c') → P c'
+
+
 
 -- Convenience lemmas you’ll use everywhere:
 
