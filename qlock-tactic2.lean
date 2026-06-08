35c7c8d0f387e9df815a158c84b270c2403685a3
byhoson <byhoson@postech.ac.kr>
2026-06-08 23:20:35 +0900

    qlock-tactic3: decouple unification from the main proof

--- qlock-tactic2.lean
+++ qlock-tactic2.lean
@@ -1,6 +1,9 @@
+/-
+Objective: automate the process of pruning non-unifiable cases with tactics
+-/
+
 import Bakery.DMC3
 import Bakery.command
-
 
 import Mathlib.Data.Multiset.Basic
 import Mathlib.Data.Multiset.AddSub
@@ -9,6 +12,11 @@ import Lean
 import Qq
 
 open Lean Meta Elab Tactic
+
+/- TODO
+* get rid of inductive types for steps and keep it simple
+* AC matching
+-/
 
 -- attempt 1: hardcoded version for _1c0
 -- macro "destruct_step" step:ident unify:ident : tactic => `(tactic|
@@ -33,7 +41,7 @@ open Lean Meta Elab Tactic
 --     done
 --   })
 
--- attempt 4: AC for multiset
+-- attempt 4: handle ::ₘ for multisets
 macro "destruct_step " step:ident unify:ident : tactic =>
   `(tactic| try {
     rcases $step:ident
@@ -53,6 +61,7 @@ structure Conf where
   q : List Nat
 instance : State Conf := ⟨⟩
 
+
 inductive step_c2n : Transition Conf where
   | mk : ∀ (i : Nat) (n w c : Multiset Nat) (q : List Nat),
       step_c2n ⟨ n, w, i ::ₘ c, i :: q ⟩ ⟨ i ::ₘ n, w, c, q ⟩
@@ -64,12 +73,17 @@ def pat1 : Pattern Conf := fun cf =>
     ∧ Multiset.Nodup (U + W)
     ∧ ↑Q = W
 
+/- NEGATIVE CASE -/
 -- ∀ st st', pat1 st → step_c2n st st' → ⊥ st'
 lemma _1c0 : (↑step_c2n : Transformer Conf) pat1 ⊥ := by
   intro s s' h_s step
   simp [pat1] at h_s
   obtain ⟨N, Q, h_unify, h_s1, h_s2⟩ := h_s
-  destruct_step step h_unify
+
+  destruct_step step h_unify -- this line covers the following 3 lines commented proof:
+  -- cases step -- { n := n✝, w := w✝, c := i✝ ::ₘ c✝, q := i✝ :: q✝ } = { n := N, w := ↑Q, c := 0, q := Q }
+  -- injection h_unify with _ _ hc _ -- hc : i✝ ::ₘ c✝ = 0
+  -- simp at hc -- contradiction found: 0 cannot be of the form i✝ ::ₘ c✝
 
 end ex1
 
@@ -153,90 +167,50 @@ example : (↑count3 : Transformer Conf) pat ⊥ := by
   destruct_step step h_unify /- it works! -/
   -- destruct_step effectively performs:
   --   cases step
-  --   injection h_unify with h1 h2 -- h1 : 3 :: L✝ = [1, n]
+  --   injection h_unify with h1 h2
   --   simp [Multiset.cons_eq_cons] at h1
 
 end ex4
 
 
-
-
-
-
+/- TODO: continue examples with AC unification -/
+namespace ex5
 
 structure Conf where
-  n : Multiset Nat
-  w : Multiset Nat
-  c : Multiset Nat
-  q : List Nat
+  numbers : Multiset Nat
+  counter : Nat
 instance : State Conf := ⟨⟩
 
-inductive step_c2n : Transition Conf where
-  | mk : ∀ (i : Nat) (n w c : Multiset Nat) (q : List Nat),
-      step_c2n ⟨ n, w, i ::ₘ c, i :: q ⟩ ⟨ i ::ₘ n, w, c, q ⟩
+inductive halve : Transition Conf where
+  | mk : ∀ (MS : Multiset Nat) (N : Nat), halve ⟨ MS + MS, N ⟩ ⟨ MS, N + 1 ⟩
 
-def pat1 : Pattern Conf := fun cf =>
-  ∃ (U W : Multiset Nat) (Q : List Nat),
-    cf = ⟨ U, W, ∅, Q ⟩
-    ∧ U + W ≠ ∅
-    ∧ Multiset.Nodup (U + W)
-    ∧ ↑Q = W
+def pat : Pattern Conf := fun cf =>
+  ∃ (N : Nat), cf = ⟨ {1,2,3} , N ⟩
 
--- ∀ st st', pat1 st → step_c2n st st' → ⊥ st'
-example : (↑step_c2n : Transformer Conf) pat1 ⊥ := by
+example : (↑halve : Transformer Conf) pat ⊥ := by
   intro s s' h_s step
-  simp [pat1] at h_s
-  obtain ⟨N, Q, h_unify, h_s1, h_s2⟩ := h_s
-
-  -- current proof state:
-  --   ...
-  --   h_unify : s = { n := N, w := ↑Q, c := 0, q := Q }
-  --   ⊢ ⊥ s'
-
-  destruct_step step h_unify -- this line covers the following 3 lines commented proof:
-  -- cases step -- { n := n✝, w := w✝, c := i✝ ::ₘ c✝, q := i✝ :: q✝ } = { n := N, w := ↑Q, c := 0, q := Q }
-  -- injection h_unify with _ _ hc _ -- hc : i✝ ::ₘ c✝ = 0
-  -- simp at hc -- contradiction found: 0 cannot be of the form i✝ ::ₘ c✝
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-inductive step_n2w : Transition Conf where
-  | mk : ∀ (i : Nat) (n w c : Multiset Nat) (q : List Nat),
-      step_n2w ⟨ i ::ₘ n, w, c, q ⟩ ⟨ n, i ::ₘ w, c, q ++ [i] ⟩
-
-inductive step_w2c : Transition Conf where
-  | mk : ∀ (i : Nat) (n w c : Multiset Nat) (q : List Nat),
-      step_w2c ⟨ n, i ::ₘ w, c, i :: q ⟩ ⟨ n, w, i ::ₘ c, i :: q ⟩
-
--- ∀ st st', pat1 st → step_n2w st st' → pat1 st'
-lemma _1a1 : (↑step_n2w : Transformer Conf) pat1 pat1 := by
-  intro s s' ps step
-  simp [pat1] at ps
-  obtain ⟨N, Q, h_unify, h_s1, h_s2⟩ := ps
-  destruct_step step h_unify -- does nothing because h_unify is unifiable
-
+  simp [pat] at h_s
+  obtain ⟨N, h_unify⟩ := h_s
+  -- destruct_step step h_unify
+  -- destruct_step effectively performs:
   cases step
-  --rcases step with ⟨i, N', W', C', Q'⟩
-  simp [pat1]
-  refine ⟨?_, ?_, ?_⟩
+  injection h_unify with h1 h2 -- h1 : MS✝ + MS✝ = 1 ::ₘ 2 ::ₘ {3}
+  simp [Multiset.cons_eq_cons, add_assoc, add_comm, add_left_comm] at h1 -- what should i add?
 
-  · sorry
-  · sorry
-  · sorry
+end ex5
 
-lemma _1b2 : (↑step_w2c : Transformer Conf) pat1 pat2 := sorry
-lemma _2a2 : (↑step_n2w : Transformer Conf) pat2 pat2 := sorry
-lemma _2b0 : (↑step_w2c : Transformer Conf) pat2 ⊥ := sorry
-lemma _2c1 : (↑step_c2n : Transformer Conf) pat2 pat1 := sorry
+namespace ex6
+
+structure Conf where
+  val : Multiset Nat
+instance : State Conf := ⟨⟩
+
+-- Rule: Requires a frame (X), plus exactly TWO {1} tokens.
+inductive dedupl : Transition Conf where
+  | mk : ∀ (X : Multiset Nat), dedupl ⟨ X + {1} + {1} ⟩ ⟨ ∅ ⟩
+
+-- Pattern: The state is strictly exactly ONE {1} token.
+def pat : Pattern Conf := fun cf =>
+  cf = ⟨ {1} ⟩
+
+end ex6
---
+++ qlock-tactic3.lean
@@ -0,0 +1,113 @@
+/-
+Objective: Decoupling unification steps from rest of the proof
+-/
+
+import Bakery.DMC3
+import Bakery.command
+
+import Mathlib.Data.Multiset.Basic
+import Mathlib.Data.Multiset.AddSub
+
+
+
+/- EXAMPLE 1 -/
+namespace ex1
+
+structure Conf where
+  n : Multiset Nat
+  w : Multiset Nat
+  c : Multiset Nat
+  q : List Nat
+instance : State Conf := ⟨⟩
+
+
+inductive step_n2w : Transition Conf where
+  | mk : ∀ (i : Nat) (n w c : Multiset Nat) (q : List Nat),
+      step_n2w ⟨ i ::ₘ n, w, c, q ⟩ ⟨ n, i ::ₘ w, c, q ++ [i] ⟩
+
+inductive step_c2n : Transition Conf where
+  | mk : ∀ (i : Nat) (n w c : Multiset Nat) (q : List Nat),
+      step_c2n ⟨ n, w, i ::ₘ c, i :: q ⟩ ⟨ i ::ₘ n, w, c, q ⟩
+
+def pat1 : Pattern Conf := fun cf =>
+  ∃ (U W : Multiset Nat) (Q : List Nat),
+    cf = ⟨ U, W, ∅, Q ⟩
+    ∧ U + W ≠ ∅
+    ∧ Multiset.Nodup (U + W)
+    ∧ ↑Q = W
+
+
+-- Generic Adapter: uses outParam to represent the unifier (positive case) / ⊥ (negative case)
+class Unify (c1 c2 : Conf) (Result : outParam Prop) where
+  resolve : c1 = c2 → Result
+
+
+
+
+/-
+  NEGATIVE CASE: non-unifiable
+-/
+
+-- unification proof
+instance {N N' W' C' : Multiset ℕ} {Q Q' : List ℕ} {i : Nat} :
+  Unify
+    { n := N', w := W', c := i ::ₘ C', q := i :: Q' }
+    { n := N, w := ↑Q, c := 0, q := Q }
+    False where
+  resolve h_unify := by
+    injection h_unify; simp_all
+
+-- ∀ st st', pat1 st → step_c2n st st' → ⊥ st'
+lemma _1c0 : (↑step_c2n : Transformer Conf) pat1 ⊥ := by
+  -- boilerplate: unpacking the pattern and the step
+  intro s s' h_s step
+  simp [pat1] at h_s
+  obtain ⟨N, Q, h_unify, h_s1, h_s2⟩ := h_s
+  rcases step
+
+  -- { n := n✝, w := w✝, c := i✝ ::ₘ c✝, q := i✝ :: q✝ } = { n := N, w := ↑Q, c := 0, q := Q }
+  obtain unifier := Unify.resolve h_unify
+
+  /- the unifier returned false, so complete the proof -/
+  assumption
+
+
+
+
+
+
+/-
+  POSITIVE CASE: unifiable
+-/
+
+-- unification proof
+instance {N N' W' C' : Multiset ℕ} {Q Q' : List ℕ} {i : Nat} :
+  Unify
+    { n := i ::ₘ N', w := W', c := C', q := Q' }
+    { n := N, w := ↑Q, c := 0, q := Q }
+    ((i ::ₘ N' = N) ∧ (W' = ↑Q) ∧ (C' = 0) ∧ (Q' = Q)) where
+  resolve h_unify := by
+    injection h_unify with hn hw hc hq
+    exact ⟨hn, hw, hc, hq⟩
+
+
+-- ∀ st st', pat1 st → step_n2w st st' → pat1 st'
+lemma _1a1 : (↑step_n2w : Transformer Conf) pat1 pat1 := by
+  -- boilerplate: unpacking the pattern and the step
+  intro s s' ps step
+  simp [pat1] at ps
+  obtain ⟨N, Q, h_unify, h_s1, h_s2⟩ := ps
+  rcases step
+
+  -- { n := i✝ ::ₘ n✝, w := w✝, c := c✝, q := q✝ } = { n := N, w := ↑Q, c := 0, q := Q }
+  obtain unifier := Unify.resolve h_unify
+
+  /- unifier exists, so continue the proof with bindings added to hypothesis -/
+  simp [pat1]
+  refine ⟨?_, ?_, ?_⟩
+
+  · sorry
+  · sorry
+  · sorry
+
+end ex1
