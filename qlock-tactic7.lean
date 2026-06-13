7ceaa5d5cf755b55d06de9fd4eb4164e0525d236
byhoson <byhoson@postech.ac.kr>
2026-06-14 06:36:21 +0900

    qlock-tactic7: implementing parser for Maude unification

--- qlock-tactic7.lean
+++ qlock-tactic7.lean
@@ -5,7 +5,7 @@ import Lean
 
 open Lean Elab Command
 
-def runMaudeUnifyQuery (filename : String) (query : String) : IO (List String) := do
+def getMaudeUnifiers (filename : String) (query : String) : IO (List String) := do
   IO.FS.withTempFile fun h queryPath => do
     h.putStr (query ++ "\n")
     h.flush
@@ -42,11 +42,115 @@ syntax "#test_maude_unify " str " with " str : command
 elab_rules : command
   | `(#test_maude_unify $filename:str with $query:str) => do
       let stdout ← liftIO <|
-        runMaudeUnifyQuery filename.getString query.getString
-      logInfo m!"Maude stdout:\n{stdout}"
+        getMaudeUnifiers filename.getString query.getString
+      logInfo m!"Maude unifiers:\n{stdout}"
 
-#test_maude_unify "tmp/multiset.maude" with
-  "variant unify X:MSet + Y:MSet =? 1 + 2 ."
+#test_maude_unify "theory/multiset.maude" with
+  "variant unify {3} =? {1} + {2} ."
 
-#test_maude_unify "tmp/multiset.maude" with
+#test_maude_unify "theory/multiset.maude" with
+  "variant unify X:MSet + Y:MSet =? {1} + {2} ."
+
+#test_maude_unify "theory/multiset.maude" with
   "variant unify X:MSet + X:MSet =? A:MSet + B:MSet ."
+
+
+def parseMaudeUnifier (u : String) : String :=
+  let stripSort (s : String) : String :=
+    match s.trim.splitOn ":" with
+    | x :: _ => x.trim
+    | [] => s.trim
+
+  let maudeExistVar? (s : String) : Option String :=
+    let x := stripSort s
+    if x.startsWith "%" then
+      some ("U" ++ x.drop 1)
+    else
+      none
+
+  let maudeAtomToLean (s : String) : String :=
+    let x := stripSort s
+    if x.startsWith "%" then
+      "U" ++ x.drop 1
+    else if x == "mt" then
+      "∅"
+    else if x.toNat?.isSome then
+      "{" ++ x ++ "}"
+    else
+      x
+
+  let maudeTermToLean (s : String) : String :=
+    s.splitOn "+"
+      |>.map maudeAtomToLean
+      |> String.intercalate " + "
+
+  let parseMaudeMapping? (line : String) : Option (String × String) :=
+    match line.splitOn "-->" with
+    | lhs :: rhs :: [] => some (lhs.trim, rhs.trim)
+    | _ => none
+
+  let varsInMaudeTerm (s : String) : List String :=
+    s.splitOn "+"
+      |>.filterMap maudeExistVar?
+
+  let uniqueStrings (xs : List String) : List String :=
+    xs.foldl
+      (fun acc x => if acc.contains x then acc else acc ++ [x])
+      []
+
+  let mappings :=
+    u.splitOn "\n"
+      |>.filterMap parseMaudeMapping?
+
+  let existVars :=
+    mappings
+      |>.flatMap (fun (_, rhs) => varsInMaudeTerm rhs)
+      |> uniqueStrings
+
+  let equations :=
+    mappings.map fun (lhs, rhs) =>
+      maudeAtomToLean lhs ++ " = " ++ maudeTermToLean rhs
+
+  let body :=
+    String.intercalate " ∧ " equations
+
+  if existVars.isEmpty then
+    body
+  else
+    "∃ " ++ String.intercalate " " existVars ++ ", " ++ body
+    --"∃ (" ++ String.intercalate " " existVars ++ " : " ++ "Multiset Nat" ++ "), " ++ body
+
+
+
+def getUnifiers
+    (lemmaName : String)
+    (binders : String)
+    (premise : String)
+    (filename : String)
+    (query : String) : IO String := do
+  let us ← getMaudeUnifiers filename query
+
+  let conclusion :=
+    match us.map (fun u => "(" ++ parseMaudeUnifier u ++ ")") with
+    | [] => "False"
+    | branches => String.intercalate " ∨ " branches
+
+  pure s!"lemma {lemmaName} {binders} : {premise} → {conclusion} := by\n  sorry"
+
+#eval do
+  IO.println <|
+    ← getUnifiers
+      "completeness"
+      "(X A B : Multiset ℕ)"
+      "X + X = A + B"
+      "theory/multiset.maude"
+      "variant unify X:MSet + X:MSet =? A:MSet + B:MSet ."
+
+-- #eval getUnifiers "theory/multiset.maude"
+--       "variant unify X:MSet + X:MSet =? A:MSet + B:MSet ."
+
+-- #eval getUnifiers "theory/multiset.maude"
+--       "variant unify X:MSet + Y:MSet =? {1} + {2} ."
+
+-- #eval getUnifiers "theory/multiset.maude"
+--       "variant unify {3} =? {1} + {2} ."
